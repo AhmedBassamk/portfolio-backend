53a18f4320ebc7ea3782a8957af4d3497abc7994
AhmedBassamk <ahmedabk2002@gmail.com>
2026-01-16 22:13:57 +0200

    add debug info route and .dockerignore

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-info', function () {
+    return response()->json([
+        'APP_NAME' => config('app.name'),
+        'APP_ENV' => config('app.env'),
+        'APP_DEBUG' => config('app.debug'),
+        'DB_CONNECTION' => config('database.default'),
+        'DB_DATABASE' => config('database.connections.sqlite.database'),
+        'DB_FILE_EXISTS' => file_exists(config('database.connections.sqlite.database')),
+        'PHP_VERSION' => phpversion(),
+        'PDO_DRIVERS' => PDO::getAvailableDrivers(),
+    ]);
+});
